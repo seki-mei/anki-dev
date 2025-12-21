1935349713b30ef209cf689a84693d34f2661cb7
seki-mei <>
2025-12-22 00:56:10 +0100

    update push-anki.sh to use new images/ location

--- push-anianki.sh
+++ push-anianki.sh
@@ -2,4 +2,4 @@
 SRC="$HOME/software/anki-dev/anianki/"
 DEST="$HOME/.var/app/net.ankiweb.Anki/data/Anki2/addons21/anianki/"
 
-rsync -av --delete --exclude '/images/' --exclude '/__pycache__/' "$SRC" "$DEST"
+rsync -av --delete --exclude '/user_files/images/' --exclude '/__pycache__/' "$SRC" "$DEST"
