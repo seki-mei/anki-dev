f5e64517fc8a4b48c4f3a6ad018051c03b73337a
seki-mei <>
2025-12-22 01:47:45 +0100

    exclude meta.json from packager

--- package-addon.sh
+++ package-addon.sh
@@ -4,4 +4,5 @@ NAME="$1"
 [ -z "$1" ] && { echo "Usage: $0 <addon-name>" >&2; exit 1; }
 cd "$BASE/$NAME" || exit 1
 rm -rf "__pycache__"
-zip -r "../$NAME.ankiaddon" *
+zip -r "../$NAME.ankiaddon" . \
+	-x "meta.json" \
