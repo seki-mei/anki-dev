d74375606a9750899c303be9c31cf3e841d88dd4
seki-mei <>
2026-01-02 14:19:11 +0100

    push-anianki delted; use package-addon.sh --open

--- push-anianki.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SRC="$HOME/software/anki-dev/anianki/"
-DEST="$HOME/.var/app/net.ankiweb.Anki/data/Anki2/addons21/anianki/"
-
-rsync -av --delete --exclude '/user_files/images/' --exclude '/__pycache__/' --exclude '/meta.json' "$SRC" "$DEST"
